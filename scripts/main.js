dada947358dcbf8da71875e2589ca8404bf44a4e
dentabones <sh183610@gmail.com>
2022-12-15 13:37:16 -0500

    Test if number clicked is an integer or starting
    Will need to convert string into number when doing a math operation

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
 "use strict";
 
 const numberButtons = document.querySelectorAll(".number");
+const operateButtons = document.querySelectorAll(".operator");
+const clearButton = document.getElementById("clear-btn");
 const display = document.querySelector("h1");
-let numberStored;
+let numberStored1;
+let numberStored2;
+let operatorStored;
+
 const addNum = (num1, num2) => num1 + num2;
 
 const subtractNum = (num1, num2) => num1 - num2;
@@ -15,10 +20,34 @@ const operate = function (operation, num1, num2) {
   return operation(num1, num2);
 };
 
-const displayClick = numberButtons.forEach((button) =>
+const displayClick1 = numberButtons.forEach((button) =>
   button.addEventListener("click", function () {
     display.textContent = button.value;
-    numberStored = button.value;
-    console.log(numberStored);
+    display.innerHTML = button.value;
+    numberStored1 = button.value;
+    console.log(numberStored1);
+    console.log(Number.isInteger(numberStored1));
   })
 );
+
+operateButtons.forEach((button) =>
+  button.addEventListener("click", function () {
+    if (button.value === "+") {
+      operatorStored = button.value;
+      display.textContent = numberStored1 + " " + operatorStored;
+    } else if (button.value === "-") {
+      operatorStored = button.value;
+      display.textContent = numberStored1 + " " + operatorStored;
+    } else if (button.value === "*") {
+      operatorStored = button.value;
+      display.textContent = numberStored1 + " " + operatorStored;
+    } else if (button.value === "/") {
+      operatorStored = button.value;
+      display.textContent = numberStored1 + " " + operatorStored;
+    }
+  })
+);
+
+clearButton.addEventListener("click", function () {
+  display.innerHTML = "0";
+});
