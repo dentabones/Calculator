6998612a80d521489628ae1b85f92616f4ff9d05
dentabones <sh183610@gmail.com>
2022-12-15 14:47:21 -0500

    Refactor variables into arrays

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,10 @@ const clearButton = document.getElementById("clear-btn");
 const display = document.querySelector("h1");
 const equalsButton = document.querySelector(".equals");
 
-let numberStored1;
-let numberStored2;
+let numberStored1 = [];
+let numberStored2 = [];
+let numA;
+let numB;
 let operatorStored;
 
 const addNum = (num1, num2) => num1 + num2;
@@ -24,14 +26,19 @@ const operate = function (operation, num1, num2) {
 
 const displayClick1 = numberButtons.forEach((button) =>
   button.addEventListener("click", function () {
-    display.textContent = button.value;
+    if (numberStored1.length !== 0 && operatorStored !== undefined) {
+      numberStored2.push(Number(button.value));
+      numB = numberStored2.join("");
+      Number(numB);
+      display.textContent = numA + operatorStored + numB;
+    } else if (operatorStored === undefined) {
+      numberStored1.push(Number(button.value));
+      numA = numberStored1.join("");
+      Number(numA);
 
-    if (numberStored1 !== undefined) {
-      numberStored2 = Number(button.value);
-      display.textContent = numberStored1 + operatorStored + numberStored2;
-    } else {
-      numberStored1 = Number(button.value);
+      display.textContent = numA;
     }
+    // display.textContent = button.value;
   })
 );
 
@@ -39,30 +46,30 @@ operateButtons.forEach((button) =>
   button.addEventListener("click", function () {
     if (button.value === "+") {
       operatorStored = button.value;
-      display.textContent = numberStored1 + " " + operatorStored;
+      display.textContent = numA + " " + operatorStored;
     } else if (button.value === "-") {
       operatorStored = button.value;
-      display.textContent = numberStored1 + " " + operatorStored;
+      display.textContent = numA + " " + operatorStored;
     } else if (button.value === "*") {
       operatorStored = button.value;
-      display.textContent = numberStored1 + " " + operatorStored;
+      display.textContent = numA + " " + operatorStored;
     } else if (button.value === "/") {
       operatorStored = button.value;
-      display.textContent = numberStored1 + " " + operatorStored;
+      display.textContent = numA + " " + operatorStored;
     }
   })
 );
 
 clearButton.addEventListener("click", function () {
   display.innerHTML = "0";
-  numberStored1 = undefined;
-  numberStored2 = undefined;
-  operatorStored = "";
+  numberStored1 = [];
+  numberStored2 = [];
+  operatorStored = undefined;
 });
 
 equalsButton.addEventListener("click", function () {
   if (operatorStored === "+") {
-    operate(addNum, numberStored1, numberStored2);
+    console.log(operate(addNum, numberStored1, numberStored2));
   } else if (operate === "-") {
     operate(subtractNum, numberStored1, numberStored2);
   } else if (operate === "*") {
